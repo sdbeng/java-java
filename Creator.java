80c9bbfd45350f87adbb0f97a86592c89a1fcd29
Sergio D. Barrera <sdbeng@gmail.com>
2021-06-24 08:32:09 -0700

    format lines

--- Creator.java
+++ Creator.java
@@ -2,7 +2,7 @@ public class Creator {
 	public static void main(String[] args) {
     		String name = "James Gosling";
     		int yearCreated = 1995;
-		System.out.println(name);
+		    System.out.println(name);
     		System.out.println(yearCreated);
 	}
 }
