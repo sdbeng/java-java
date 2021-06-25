bd6db343247c08a04412b8df73b70b2945ee8e52
Sergio D. Barrera <sdbeng@gmail.com>
2021-06-25 07:49:05 -0700

    update Song class

--- Creator.java
+++ Creator.java
@@ -2,7 +2,20 @@ public class Creator {
 	public static void main(String[] args) {
     		String name = "James Gosling";
     		int yearCreated = 1995;
+            // double num = "60";//cannot convert String to double
+
+            //check for compilation errors
+            String gradeOnTest = "A";
+            int balance = -30;
+            //double isRaining = false;//type mistmatch: cannot convert from boolean to double
+            char grade_on_test = 'F';
+            int num = (10 -(4 + 3)) * 6; //18
+
 		    System.out.println(name);
     		System.out.println(yearCreated);
+    		System.out.println(num);
 	}
 }
+
+/*Using the order of operations, (4 + 3) is evaluated first. Then, (10 - 7) is evaluated. Finally, 3 * 6 is evaluated producing a value of 18.
+*/
--- Song.java
+++ Song.java
@@ -1,3 +1,10 @@
+/* 
+A variable starts with a valid letter, or a $, or a _. No other symbols or numbers can begin a variable name. 1stPlace and *Gazer are not valid variable names.
+
+valid var names: The first letter is uncapitalized, there are no underscores, and each non-starting word is capitalized. 
+
+Variable names of only one word are spelled in all lowercase letters. Variable names of more than one word have the first letter lowercase while the beginning letter of each subsequent word is capitalized. This style of capitalization is called camelCase.
+*/
 public class Song {
 	public static void main(String[] args) {   
     String openingLyrics = "Yesterday, all my troubles seemed so far away";
